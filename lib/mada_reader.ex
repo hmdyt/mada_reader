f4ae94161d0fb353c79151450296d9e397239937
hmdyt <konnyaku108@gmail.com>
2022-06-30 18:58:13 +0900

    add output file arg

--- lib/mada_reader.ex
+++ lib/mada_reader.ex
@@ -3,15 +3,33 @@ defmodule MadaReader do
 
   def main(argv) do
     argv
-    |> Enum.at(0)
-    |> run
+    |> parse_args()
   end
 
-  def run(path_to_mada \\ "/Users/yuto/sandbox/mada_reader/per0014/GBKB-13_0003.mada") do
+  def parse_args(argv) do
+    argv
+    |> parse_args_helper()
+    |> run()
+  end
+
+  defp parse_args_helper([inputfile]), do: [inputfile]
+  defp parse_args_helper([inputfile, outputfile]), do: [inputfile, outputfile]
+  defp parse_args_helper(_) do
+    IO.puts "Usage: mada_reader <input.mada> [output.root]"
+    System.halt(1)
+  end
+
+  def run([path_to_mada]) do
     path_to_mada
     |> MadaReader.MadaRead.run
     |> MadaStructure.write(path_to_mada |> String.replace(".mada", ".tmp"))
     |> make_tree()
+  end
+  def run([path_to_mada, path_to_output]) do
+    path_to_mada
+    |> MadaReader.MadaRead.run()
+    |> MadaStructure.write(path_to_output)
+    |> make_tree(path_to_output)
   end
 
   def make_tree(path_to_tmp_file) do
@@ -19,11 +37,8 @@ defmodule MadaReader do
     {ret, _status} = System.cmd(@make_tree_binary, [path_to_tmp_file, path_to_out_file])
     IO.puts ret
   end
+  def make_tree(path_to_tmp_file, path_to_output_file) do
+    {ret, _status} = System.cmd(@make_tree_binary, [path_to_tmp_file, path_to_output_file])
+    IO.puts ret
+  end
 end
-
-# TODO
-# - docker
-# - [DONE] root macro compile (make)
-# - elixir single binary
-# - delete hardcode
-# - multi threading (on perXXXX)
