a8ccc7fe024ad7ca8ee4dd0cf8871ebdf9c1c120
hmdyt <konnyaku108@gmail.com>
2022-07-03 05:57:58 +0900

    change bin dir

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 
 export PROJECT_ROOT=$(pwd)
+path_to_bin=$PROJECT_ROOT/bin
 
 cd rootmacro/build
-cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_ROOT/rootmacro/bin ..
+cmake -DCMAKE_INSTALL_PREFIX=$path_to_bin ..
 make install
 cd - > /dev/null
-export PATH=$PROJECT_ROOT/rootmacro/bin:$PATH
+export PATH=$path_to_bin:$PATH
 
 mix deps.get
 mix escript.build
+cp mada_reader $path_to_bin/.
+rm mada_reader
